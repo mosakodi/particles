591de8a7e3774e36131fede09d6c122fcef647ef
mosakodi <mosakodi@gmail.com>
2022-02-21 20:51:08 -0800

    2 images in a particle array

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 let particles = [];
-let earthrise;
+let earths = [];
+
 
 
 function setup() {
 	createCanvas(800, 800);
-	earthrise = loadImage('assets/earthrise.PNG')
+	earths[0] = loadImage('assets/earthrise.PNG')
+	earths[1] = loadImage('assets/earthmoon.PNG')
 	for (var i = 0; i < 100; i++) {
 		particles.push(new Particle);
 	}
@@ -25,10 +27,19 @@ class Particle{//what do we do with this data, we hold it here
 		this.size = random(5, 50);
 		this.xSpeed = random(-1, 1);
 		this.ySpeed = random(-1, 1);
+		this.image = int(random(2));
 	}
 
 	display(){//we can display whatever we want ellipse, rect, images, etc
-	image(earthrise, this.x, this.y, this.size, this.size);
+		let newSize;
+		if(this.image == 1){
+		newSize = this.size * 2;
+	}
+	else{
+		newSize = this.size;
+	}
+	image(earths[this.image], this.x, this.y, this.size, this.size);
+	
 }
 
 move(){
