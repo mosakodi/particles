119ba9a1dd9c81a64f725d1e0c93b7f634facfdd
mosakodi <mosakodi@gmail.com>
2022-03-07 16:12:40 -0800

    images, moon function, particles and text in place

--- sketch.js
+++ sketch.js
@@ -1,55 +1,42 @@
 let img1;
-var angle = 0;
-// let particles = [];
-// let newmoon = [];
-// let crescent = [];
-// let firstquarter = [];
-// let gibbous = [];
-// let fullmoon = [];
-// let disseminating = [];
-// let balsamic = [];
-
+var angle = -225;
+let particles = [];
+let newmoon = [];
+let crescent = [];
+let firstquarter = [];
+let gibbous = [];
+let fullmoon = [];
+let disseminating = [];
+let lastquarter = [];
+let balsamic = [];
 
 function preload() {
-  // img1 = loadImage('assets/outerwheel4.png');
-  img1 = loadImage('assets/innerwheel2.png');
-  // img3 = loadImage('assets/innerwheel2.png');
-  // img4 = loadImage('assets/innerwheel2.png');
-  // img5 = loadImage('assets/innerwheel2.png');
-  // img6 = loadImage('assets/innerwheel2.png');
-  // img7 = loadImage('assets/innerwheel2.png');
-  // img8 = loadImage('assets/innerwheel2.png');
-  // img9 = loadImage('assets/innerwheel2.png');
-  // img10 = loadImage('assets/innerwheel2.png');
-  // img11 = loadImage('assets/innerwheel2.png');
-  // img12 = loadImage('assets/innerwheel2.png');
-  // img13 = loadImage('assets/innerwheel2.png');
-  // img14 = loadImage('assets/innerwheel2.png');
-
-}
+  img1 = loadImage('assets/graph4.png');
+ }
 
 function setup() {
  createCanvas (800,800);
- // newmoon[0] = loadImage('assets/earthrise.PNG')
- // newmoon[1] = loadImage('assets/earthmoon.PNG')
- // // crescent[0] = loadImage('assets/innerwheel2.png');
- // // crescent[1] = loadImage('assets/innerwheel2.png');
- // // firstquarter[0] = loadImage('assets/innerwheel2.png');
- // // firstquarter[1] = loadImage('assets/innerwheel2.png');
- // // gibbous[0] = loadImage('assets/innerwheel2.png');
- // // gibbous[1] = loadImage('assets/innerwheel2.png');
- // // fullmoon[0 = loadImage('assets/innerwheel2.png');
- // // fullmoon[1]] = loadImage('assets/innerwheel2.png');
- // // disseminating[0] = loadImage('assets/innerwheel2.png');
- // // disseminating[1] = loadImage('assets/innerwheel2.png');
- // // lastquarter[0] = loadImage('assets/innerwheel2.png');
- // // lastquarter[1] = loadImage('assets/innerwheel2.png');
- // // balsamic[0] = loadImage('assets/innerwheel2.png');
- // // balsamic[1] = loadImage('assets/innerwheel2.png');
+ newmoon[0] = loadImage('assets/moon1.png');
+ newmoon[1] = loadImage('assets/moon2.png');
+ crescent[0] = loadImage('assets/earthwobble.png');
+ crescent[1] = loadImage('assets/moon3.png');
+ firstquarter[0] = loadImage('assets/venus2.png');
+ firstquarter[1] = loadImage('assets/venus3.png');
+ gibbous[0] = loadImage('assets/laussel.png');
+ gibbous[1] = loadImage('assets/lausselmoon.png');
+ fullmoon[0] = loadImage('assets/z1.png');
+ fullmoon[1] = loadImage('assets/z2.png');
+ disseminating[0] = loadImage('assets/catal1.png');
+ disseminating[1] = loadImage('assets/catal2.png');
+ lastquarter[0] = loadImage('assets/firstauthor.png');
+ lastquarter[1] = loadImage('assets/firstauthorround.png');
+ balsamic[0] = loadImage('assets/mothermary.png');
+ balsamic[1] = loadImage('assets/lilith.png');
+ textAlign(LEFT);
 
- // for (var i = 0; i < 100; i++) {
-	// 	particles.push(new Particle);
-	// }
+ for (var i = 0; i < 100; i++) {
+		particles.push(new Particle);
+	}
 }
 
 function draw() {
@@ -61,73 +48,109 @@ function draw() {
   	image(img1, 0, 0);
   	pop();
 
-	// angle+=-0.0025
+ textAlign(LEFT);
+ textSize(25);
+ fill(255);
+
 	angle+= -0.1;
 	angle = angle%360;
 
   	moon(angle);
-  	// print(angle);
+  	print(angle);
 
-  	// if(angle < -45 && angle > -46){//trigger new moon
-  	// 	print('respawn');
-  	// 	for (var i = 0; i < particles.length; i++) {
-  	// 		particles[i].respawn(0);//0 = sun in this case
-  	// 	}
-  	// }
+  	if(angle < -225 && angle > -226){//trigger new moon
+  		print('respawn');
+  		for (var i = 0; i < particles.length; i++) {
+  			particles[i].respawn(0);
+  		}
+  	}
 
-  	// if(angle < -90 && angle > -91){//trigger crescent
-  	// 	print('respawn');
-  	// 	for (var i = 0; i < particles.length; i++) {
-  	// 		particles[i].respawn(0);//0 = sun in this case
-  	// 	}
-  	// }
+  	if(angle < -225 && angle > -265){
+  		text('earth & theia collide to birth the moon ~  4.5 billion years BCE', 10, 25);
+  	}
 
-  	// if(angle < -135 && angle > -136){//trigger firstquarter
-  	// 	print('respawn');
-  	// 	for (var i = 0; i < particles.length; i++) {
-  	// 		particles[i].respawn(0);//0 = sun in this case
-  	// 	}
-  	// }
+  	if(angle < -270 && angle > -271){//trigger crescent
+  		print('respawn');
+  		for (var i = 0; i < particles.length; i++) {
+  			particles[i].respawn(1);
+  		}
+  	}
 
-  	// if(angle < -180 && angle > -181){//trigger gibbous
-  	// 	print('respawn');
-  	// 	for (var i = 0; i < particles.length; i++) {
-  	// 		particles[i].respawn(0);//0 = sun in this case
-  	// 	}
-  	// }
+  	if(angle < -270 && angle > -310){
+  		text('moon limits earth wobble, enabling organic life ~ ongoing', 10, 25);
+  	}
 
-  	// if(angle < -225 && angle > -226){//trigger fullmoon
-  	// 	print('respawn');
-  	// 	for (var i = 0; i < particles.length; i++) {
-  	// 		particles[i].respawn(0);//0 = sun in this case
-  	// 	}
-  	// }
+  	if(angle < -315 && angle > -316){//trigger firstquarter
+  		print('respawn');
+  		for (var i = 0; i < particles.length; i++) {
+  			particles[i].respawn(2);
+  		}
+  	}
 
-  	// if(angle < -270 && angle > -271){//trigger disseminating
-  	// 	print('respawn');
-  	// 	for (var i = 0; i < particles.length; i++) {
-  	// 		particles[i].respawn(0);//0 = sun in this case
-  	// 	}
-  	// }
+  	if(angle < -315 && angle > -355){
+  		text('human celebration of life: Venus of Willendorf ~ 25,000 BCE', 10, 25);
+  	}
 
-  	// if(angle < -335 && angle > -336){//trigger lastquarter
-  	// 	print('respawn');
-  	// 	for (var i = 0; i < particles.length; i++) {
-  	// 		particles[i].respawn(0);//0 = sun in this case
-  	// 	}
-  	// }
+  	if(angle < -0 && angle > -1){//trigger gibbous
+  		print('respawn');
+  		for (var i = 0; i < particles.length; i++) {
+  			particles[i].respawn(3);
+  		}
+  	}
 
-  	// if(angle < -0 && angle > -1){//trigger balsamic
-  	// 	print('respawn');
-  	// 	for (var i = 0; i < particles.length; i++) {
-  	// 		particles[i].respawn(0);//0 = sun in this case
-  	// 	}
-  	// }
+  	if(angle < -0 && angle > -40){
+  		text('connecting moon, time & human life: Venus of Laussel ~ 23,000 BCE', 10, 25);
+  	}
 
-  	// for (var i = 0; i < particles.length; i++) {
-  	// 	particles[i].display();
-  	// 	particles[i].move();
-  	// }
+  	if(angle < -45 && angle > -46){//trigger fullmoon
+  		print('respawn');
+  		for (var i = 0; i < particles.length; i++) {
+  			particles[i].respawn(4);
+  		}
+  	}
+
+  	if(angle < -45 && angle > -85){
+  		text('connecting human & animal life: Zaraysk Venuses ~ 18,000 BCE', 10, 25);
+  	}
+
+
+  	if(angle < -90 && angle > -91){//trigger disseminating
+  		print('respawn');
+  		for (var i = 0; i < particles.length; i++) {
+  			particles[i].respawn(5);
+  		}
+  	}
+
+  	if(angle < -90 && angle > -130){
+  		text('giving thanks: Grain Goddess of Catal Huyuk ~ 8,000 BCE', 10, 25);
+  	}
+
+  	if(angle < -135 && angle > -136){//trigger lastquarter
+  		print('respawn');
+  		for (var i = 0; i < particles.length; i++) {
+  			particles[i].respawn(6);
+  		}
+  	}
+
+    if(angle < -135 && angle > -175){//trigger new moon
+  		text('Enheduanna, high priestess of the Mesopotemian moongod ~ 3,000 BCE', 10, 25);
+  	}
+
+  	if(angle < -180 && angle > -181){//trigger balsamic
+  		print('respawn');
+  		for (var i = 0; i < particles.length; i++) {
+  			particles[i].respawn(7);
+  		}
+  	}
+
+  	if(angle < -180 && angle > -220){
+  		text('biblical perspective: Lilith & Mother Mary ~ start of CE', 10, 25);
+  	}
+
+  	for (var i = 0; i < particles.length; i++) {
+  		particles[i].display();
+  		particles[i].move();
+  	}
   }
 
   function moon(phase){
@@ -139,56 +162,58 @@ function draw() {
 	line(0, 0, 130, 130);
 	pop();
 }
-// class Particle{//what do we do with this data, we hold it here
-// 	 constructor(){
-// 		this.size = random(5, 50);
-// 		this.xSpeed = random(-2, 2);
-// 		this.ySpeed = random(-2, 2);
-// 		this.image = int(random(2));
-// 		this.x = width/2;
-// 		this.y = height/2;
-// 		this.moonphase = 0;
-// 		this.visible = false;
-// 	}
 
-// 	display(){
-// 		if (this.visible){
-// 			if(this.moonphase == 0){
-// 				image(newmoon[this.image], this.x, this.y, this.size, this.size);
-// 			}
-// 			if(this.moonphase == 1){
-// 				image(crescent[this.image], this.x, this.y, this.size, this.size);//put diff image array here
-// 			}
-			// if(this.moonphase == 2){
-// 				image(firstquarter[this.image], this.x, this.y, this.size, this.size);//put diff image array here
-// 			}
-			// if(this.moonphase == 3){
-// 				image(gibbous[this.image], this.x, this.y, this.size, this.size);//put diff image array here
-// 			}
-			// if(this.moonphase == 4){
-// 				image(full[this.image], this.x, this.y, this.size, this.size);//put diff image array here
-// 			}
-			// if(this.moonphase == 5){
-// 				image(disseminating[this.image], this.x, this.y, this.size, this.size);//put diff image array here
-// 			}
-			// if(this.moonphase == 6){
-// 				image(lastquarter[this.image], this.x, this.y, this.size, this.size);//put diff image array here
-// 			}
-			// if(this.moonphase == 7){
-// 				image(balsamic[this.image], this.x, this.y, this.size, this.size);//put diff image array here
-// 			}
-// 		}
 
-// 	}
+class Particle{
+	 constructor(){
+		this.size = random(100, 150);
+		this.xSpeed = random(-2, 2);
+		this.ySpeed = random(-2, 2);
+		this.image = int(random(2));
+		this.x = width/2;
+		this.y = height/2;
+		this.moonphase = 0;
+		this.visible = false;
+	}
 
-// 	move(){
-// 		this.x = this.x + this.xSpeed;
-// 		this.y = this.y + this.ySpeed;
-// 	}
+	display(){
+		if (this.visible){
+			if(this.moonphase == 0){
+				image(newmoon[this.image], this.x, this.y, this.size, this.size);
+			}
+			if(this.moonphase == 1){
+				image(crescent[this.image], this.x, this.y, this.size, this.size);
+			}
+			if(this.moonphase == 2){
+				image(firstquarter[this.image], this.x, this.y, this.size, this.size);
+			}
+			if(this.moonphase == 3){
+				image(gibbous[this.image], this.x, this.y, 150, 200);
+			}
+			if(this.moonphase == 4){
+				image(fullmoon[this.image], this.x, this.y, this.size, this.size);
+			}
+			if(this.moonphase == 5){
+				image(disseminating[this.image], this.x, this.y, 100, 150);
+			}
+			if(this.moonphase == 6){
+				image(lastquarter[this.image], this.x, this.y, this.size, this.size);
+			}
+			if(this.moonphase == 7){
+				image(balsamic[this.image], this.x, this.y, this.size, this.size);
+			}
+		}
+	}
 
-// 	respawn(moonphaseNumber){//
-// 		this.visible = true;
-// 		this.moonphase = moonphaseNumber;
-// 		this.x = width/2;
-// 		this.y = height/2;
-// 	}
+	move(){
+		this.x = this.x + this.xSpeed;
+		this.y = this.y + this.ySpeed;
+	}
+
+	respawn(moonphaseNumber){
+		this.visible = true;
+		this.moonphase = moonphaseNumber;
+		this.x = width/2;
+		this.y = height/2;
+	}
+}
